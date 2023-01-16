41c08364a25e4155b53e6b9400fd77b31c780aa6
huuquan2952001 <huuquan2952001@gmail.com>
2023-01-16 08:47:03 +0700

    add email item

--- src/component/EmailItem/index.js
+++ src/component/EmailItem/index.js
@@ -4,38 +4,46 @@ import StarIcon from "@mui/icons-material/Star";
 import { useState } from "react";
 import DraftsOutlinedIcon from "@mui/icons-material/DraftsOutlined";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import clsx from "clsx";
 
-function EmailItem() {
+function EmailItem({ item, checked, onCheck }) {
   const [showStar, setShowStar] = useState(false);
   return (
-    <div className={styles.emailItem}>
-      <div className={styles.checkbox}>
-        <input type="checkbox" />
+    <div className={clsx(styles.emailItem, checked && styles.active)}>
+      <div className={clsx(styles.checkbox, checked && styles.active)}>
+        <input
+          type="checkbox"
+          onChange={() => onCheck(checked, item.id)}
+          checked={checked}
+        />
       </div>
-      <button className={styles.starred} onClick={() => setShowStar(!showStar)}>
+      <button
+        className={clsx(styles.starred, checked && styles.active)}
+        onClick={() => setShowStar(!showStar)}
+      >
         {showStar ? (
           <StarIcon className={styles.iconStarYellow} />
         ) : (
           <StarOutlineIcon className={styles.iconStar} />
         )}
       </button>
-      <div className={styles.sender}>Lehuuquan</div>
+      <div className={styles.sender}>{item.Name}</div>
       <div className={styles.emailContent}>
         <span className={styles.contentDetails}>
-          <span className={styles.contentTitle}>
-            Bạn có hài lòng với công việc hiện tại không? -
-          </span>
-          Tham gia khảo sát, có ngay cơ hội nhận phần quà là tiền mặt 200K Bạn
-          thân mến, JobsGO xin cảm ơn bạn đã đồng hành cùng JobsGO trong suốt
-          thời gian vừa qua. Là một đơn vị uy tín trong lĩnh vực tuyển
+          <span className={styles.contentTitle}>{item.Title} -</span>
+          {item.Content}
         </span>
       </div>
-      <div className={styles.emailTime}>09:27</div>
+      <div className={styles.emailTime}>{item.Time}</div>
       <div className={styles.emailItemDashbroad}>
-        <button className={styles.dashbroadIcon}>
+        <button
+          className={clsx(styles.dashbroadIcon, checked && styles.active)}
+        >
           <DeleteOutlineIcon className={styles.icon} />
         </button>
-        <button className={styles.dashbroadIcon}>
+        <button
+          className={clsx(styles.dashbroadIcon, checked && styles.active)}
+        >
           <DraftsOutlinedIcon className={styles.icon} />
         </button>
       </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,12 @@
 import EmailItem from "../../component/EmailItem";
+import EmailList from "../../component/EmailList";
 import styles from "./Home.module.scss";
 
 function Home() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.topbar}>
-        <div>
-          <input type="checkbox" />
-        </div>
-        <div>
-          <span>1-50 of 155</span>
-        </div>
-      </div>
       <div className={styles.menuEmail}>
-        <EmailItem />
+        <EmailList />
       </div>
     </div>
   );
